8dd08da62a34c17b475af5aa3ba0cd2dbeb91876
Naresh Chaurasia <johndoe@example.com>
2025-05-20 07:02:01 +0530

    first commit

---
+++ user-microservice/src/main/java/in/all/StartUserMS.java
@@ -0,0 +1,14 @@
+package in.all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class StartUserMS {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartUserMS.class, args);
+    }
+}
