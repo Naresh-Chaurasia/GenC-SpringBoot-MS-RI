a3ed205f6df70bdeb283f65b6fca7fe698c460f6
Naresh Chaurasia <johndoe@example.com>
2025-07-04 11:51:40 +0530

    Macbook Pro, Checkin Timestamp::Fri Jul  4 11:51:40 IST 2025

--- user-microservice-react/src/main/java/in/all/StartUserMS.java
+++ user-microservice-react/src/main/java/in/all/StartUserMS.java
@@ -2,7 +2,7 @@ package in.all;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 //@EnableDiscoveryClient
